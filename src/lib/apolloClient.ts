b4aff28308ac1beae96aa29de0a8d62177b508d3
Eman <emmanuelgatwech@gmail.com>
2022-03-09 07:56:41 +0300

    chore: add pre-commit hooks

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,147 +1,154 @@
-import { ApolloClient, InMemoryCache, createHttpLink, NormalizedCacheObject } from '@apollo/client'
-import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
-import { split } from '@apollo/client'
-import { getMainDefinition } from '@apollo/client/utilities'
-import { onError } from '@apollo/client/link/error'
-import { createClient } from 'graphql-ws'
+import {
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+  NormalizedCacheObject,
+} from "@apollo/client";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { split } from "@apollo/client";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { onError } from "@apollo/client/link/error";
+import { createClient } from "graphql-ws";
 
-let apolloClient: ApolloClient<NormalizedCacheObject> | null = null
-const isBrowser = typeof window !== 'undefined'
+let apolloClient: ApolloClient<NormalizedCacheObject> | null = null;
+const isBrowser = typeof window !== "undefined";
 
 type HttpOptions = {
   headers?: {
-    Authorization?: string
-    'x-hasura-admin-secret'?: string
-    'x-hasura-role'?: string
-  }
-  token?: string
-}
-
-
-const errorLink = onError(({ graphQLErrors, networkError }) => {
-  if (graphQLErrors) {
-    graphQLErrors.map(({ message: msg, extensions }) => {
-      console.error('graphql error', msg)
-      switch (extensions?.['code']) {
-        case 'invalid-jwt':
-          // perform logout
-          break
-        default:
-          return
-      }
-    })
-  }
-  if (networkError) {
-    console.error(`Network Error: ${networkError.message}`)
-  }
-})
+    Authorization?: string;
+    "x-hasura-admin-secret"?: string;
+    "x-hasura-role"?: string;
+  };
+  token?: string;
+};
 
 const createLink = (opts: HttpOptions = {}) => {
   return createHttpLink({
     uri: process.env.NEXT_PUBLIC_HASURA_ENDPOINT,
-    credentials: 'include',
+    credentials: "include",
     headers: {
       ...opts.headers,
-      'x-hasura-admin-secret': process.env.NEXT_PUBLIC_HASURA_GRAPHQL_ADMIN_SECRET,
-      'x-hasura-role': 'user',
-      Authorization: 'secret'
-    }
-  })
-}
+      "x-hasura-admin-secret":
+        process.env.NEXT_PUBLIC_HASURA_GRAPHQL_ADMIN_SECRET,
+      "x-hasura-role": "user",
+      Authorization: "secret",
+    },
+  });
+};
+
+const httpLink = createLink({
+  headers: {
+    "x-hasura-admin-secret":
+      process.env.NEXT_PUBLIC_HASURA_GRAPHQL_ADMIN_SECRET,
+    "x-hasura-role": "user",
+    Authorization: "secret",
+  },
+});
+
+const wsLink = isBrowser
+  ? new GraphQLWsLink(
+      createClient({
+        url: "ws://frodle.hasura.app/v1/graphql",
+        connectionParams: {
+          headers: {
+            "x-hasura-admin-secret":
+              process.env.NEXT_PUBLIC_HASURA_GRAPHQL_ADMIN_SECRET,
+            Authorization: "secret",
+            "x-hasura-role": "user",
+          },
+        },
+      })
+    )
+  : httpLink;
+
+const splitLink = isBrowser
+  ? split(
+      ({ query }) => {
+        const definition = getMainDefinition(query);
+        return (
+          definition.kind === "OperationDefinition" &&
+          definition.operation === "subscription"
+        );
+      },
+      wsLink,
+      httpLink
+    )
+  : httpLink;
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.map(({ message: msg, extensions }) => {
+      console.error("graphql error", msg);
+      switch (extensions?.["code"]) {
+        case "invalid-jwt":
+          // perform logout
+          break;
+        default:
+          return;
+      }
+    });
+  }
+  if (networkError) {
+    console.error(`Network Error: ${networkError.message}`);
+  }
+});
 
 interface Options {
-  token?: string
+  token?: string;
 }
 export const setAuthToken = (token: string) => {
   const options: HttpOptions = {
     headers: {
-        'x-hasura-admin-secret': process.env.NEXT_PUBLIC_HASURA_GRAPHQL_ADMIN_SECRET,
-        'x-hasura-role': 'user',
-        Authorization: 'secret'
-    }
-  }
+      "x-hasura-admin-secret":
+        process.env.NEXT_PUBLIC_HASURA_GRAPHQL_ADMIN_SECRET,
+      "x-hasura-role": "user",
+      Authorization: "secret",
+    },
+  };
 
   if (apolloClient) {
-    const newLink = createLink(options)
+    const newLink = createLink(options);
 
-    apolloClient.resetStore()
-    apolloClient.setLink(errorLink.concat(newLink))
+    apolloClient.resetStore();
+    apolloClient.setLink(errorLink.concat(newLink));
   }
-}
-
-
+};
 
 function create(initialState: any, options: Options = {}) {
   const client = new ApolloClient({
     connectToDevTools: !isBrowser,
     ssrMode: isBrowser, // Disables forceFetch on the server (so queries are only run once)
-    link: createLink(options),
+    link: splitLink,
     cache: new InMemoryCache().restore(initialState || {}),
     defaultOptions: {
       watchQuery: {
-        fetchPolicy: 'cache-and-network'
-      }
-    }
-  })
+        fetchPolicy: "cache-and-network",
+      },
+    },
+  });
 
   if (!isBrowser) {
-    const httpLink = createLink({
-      headers: {
-        'x-hasura-admin-secret': process.env.NEXT_PUBLIC_HASURA_GRAPHQL_ADMIN_SECRET,
-        'x-hasura-role': 'user',
-        Authorization: 'secret'
-      }
-    })
-
-    client.setLink(httpLink)
+    client.setLink(httpLink);
   }
 
-  if (isBrowser) {
-
-    const wsLink = new GraphQLWsLink(createClient({
-      url: 'wss://frodle.hasura.app/v1/subscriptions',
-    }));
-
-    const httpLink = createLink({
-      headers: {
-        'x-hasura-admin-secret': process.env.NEXT_PUBLIC_HASURA_GRAPHQL_ADMIN_SECRET,
-        'x-hasura-role': 'user',
-        Authorization: 'secret'
-      }
-    })
-
-    const splitLink = split(
-      ({ query }) => {
-        const definition = getMainDefinition(query);
-        return (
-          definition.kind === 'OperationDefinition' &&
-          definition.operation === 'subscription'
-        );
-      },
-      wsLink,
-      httpLink,
-    );
-
-    client.setLink(splitLink)
-  }
-   
-
-
-  return client
+  return client;
 }
 
-export default function initApollo(initialState: Record<string, any> = {}, options?: Options) {
+export default function initApollo(
+  initialState: Record<string, any> = {},
+  options?: Options
+) {
   // Make sure to create a new client for every server-side request so that data
   // isn't shared between connections (which would be bad)
   if (!isBrowser) {
-    return create(initialState, options)
+    return create(initialState, options);
   }
   // Reuse client on the client-side
   if (!apolloClient) {
-    apolloClient = create(initialState, options)
+    apolloClient = create(initialState, options);
   }
 
-  return apolloClient
+  return apolloClient;
 }
 
-export const client = initApollo()
+export const client = initApollo();
--- src/machine.ts
+++ src/machine.ts
@@ -1,69 +1,62 @@
-import { createMachine } from 'xstate'
+import { createMachine } from "xstate";
 
 export type Game = {
-    rounds: any
-    players: any
-    letters: any
-    turn: null
-    winner: null,
-    leaderBoard: any
-}
+  rounds: any;
+  players: any;
+  letters: any;
+  turn: null;
+  winner: null;
+  leaderBoard: any;
+};
 
 export const gameMachine = () => {
-    return function createGameMachine (game: Game) {
-        return createMachine({
-            id: 'game',
-            initial: 'home',
-            context: {
-                ...game
-            },
-            states: {
-              home: {
-                  on: {
-                      CLICK_PLAY: 'mode',
-                      CLICK_OPTIONS: 'options',
-                      CLICK_GUIDE: 'guide'
-                  }
-              },
-              started: {
-                  on: {
-                      START: {
-        
-                      }
-                  },
-                  onDone: {
-        
-                  }
-              },
-              options: {},
-              guide: {
-        
-              },
-              menu: {},
-              mode: {
-                  on: {
-                      CREATE_ROOM: 'lobby',
-                      JOIN_ROOM: 'room'
-                  }
-              },
-              room: {
-                  on: {
-                      NEW_ROOM: 'mode',
-                      EXISTING_ROOM: 'lobby'
-                  }
-              },
-              lobby: {
-                  on: {
-                    SEND_MESSAGE: '',
-                    VIEW_PROFILE: '',
-                    CREATE_ROOM: '',
-                    START_GAME: ''
-                  }
-        
-              },
-              gameplay: {},
-              over: {}
-            }
-          })
-    }
-}
+  return function createGameMachine(game: Game) {
+    return createMachine({
+      id: "game",
+      initial: "home",
+      context: {
+        ...game,
+      },
+      states: {
+        home: {
+          on: {
+            CLICK_PLAY: "mode",
+            CLICK_OPTIONS: "options",
+            CLICK_GUIDE: "guide",
+          },
+        },
+        started: {
+          on: {
+            START: {},
+          },
+          onDone: {},
+        },
+        options: {},
+        guide: {},
+        menu: {},
+        mode: {
+          on: {
+            CREATE_ROOM: "lobby",
+            JOIN_ROOM: "room",
+          },
+        },
+        room: {
+          on: {
+            NEW_ROOM: "mode",
+            EXISTING_ROOM: "lobby",
+          },
+        },
+        lobby: {
+          on: {
+            SEND_MESSAGE: "",
+            VIEW_PROFILE: "",
+            CREATE_ROOM: "",
+            START_GAME: "",
+          },
+        },
+        gameplay: {},
+        over: {},
+      },
+    });
+  };
+};
